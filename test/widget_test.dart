d8db9b84f17ec41c0af1c4280329153ebdab9c14
Paquito35 <thomas.strinati@gmail.com>
2024-03-14 14:47:41 +0100

    V3bis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dashboard_bourse_v2/main.dart';
+import 'package:dashboard_bourse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
