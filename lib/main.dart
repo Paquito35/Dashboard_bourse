39fdd775a4b03478e4d8e3eb348829832f8fe94d
Paquito35 <thomas.strinati@gmail.com>
2024-03-27 18:25:18 +0100

    dictionnaire et API fonctionnelle

--- lib/main.dart
+++ lib/main.dart
@@ -40,40 +40,7 @@ class _bourse_dashboardState extends State<bourse_dashboard> {
     loadJsonData('AAPL_data'); // Load AAPL data by default
     loadAllDataAndCalculate(); // load best and worst percentage
   }
-  Map<String, String> dicSymbol = {
-    'Apple Inc.': 'aapl_data',
-    'Amazon Inc.': 'amzn_data',
-    'Groupe AXA': 'cs.pa_data',
-    'The Walt Disney Company': 'dis_data',
-    'Google LLC': 'goog_data',
-    'Coca-Cola Company': 'ko_data',
-    'Meta Platforms Inc.': 'meta_data',
-    'Bitcoin': 'btc-usd_data',
-    'Microsoft Corporation': 'msft_data',
-    'Netflix Inc.': 'nflx_data',
-    'NVIDIA Corporation': 'nvda_data',
-    'Tesla Inc.': 'tsla_data',
-    'Visa Inc.': 'v_data',
-    'LVMH': 'mc.pa_data',
-    'ACCOR S.A.': 'ac.pa_data',
-    'TotalEnergies SE': 'tte.pa_data',
-    'Sanofi S.A.': 'san.pa_data',
-    "L'Oréal S.A.": 'or.pa_data',
-    'Airbus SE': 'air.pa_data',
-    'BNP Paribas S.A.': 'bnp.pa_data',
-    'Société Générale S.A.': 'gle.pa_data',
-    'Orange S.A.': 'ora.pa_data',
-    'Renault S.A.': 'rno.pa_data',
-    'Michelin SCA': 'ml.pa_data',
-    'Thales Group': 'ho.pa_data',
-    'Capgemini SE': 'cap.pa_data',
-    'Groupe Carrefour': 'ca.pa_data',
-    'Veolia Environnement SA': 'vie.pa_data',
-    'VINCI S.A.': 'dg.pa_data',
-    'Groupe Danone': 'bn.pa_data',
-    'Hermès International': 'rms.pa_data',
-    'Air Liquide': 'ai.pa_data',
-  };
+  Map<String, String> dicSymbol = {'3M Company': 'MMM', 'ACCOR S.A.': 'ac.pa_data', 'ASML Holding N.V.': 'ASML', 'AT&T Inc.': 'T', 'AbbVie Inc.': 'ABBV', 'Adidas AG': 'ADDYY', 'Adobe Inc.': 'ADBE', 'Affirm Holdings, Inc.': 'AFRM', 'Agricultural Bank of China Limited': 'ACGBY', 'Air Liquide': 'ai.pa_data', 'Air Products and Chemicals, Inc.': 'APD', 'Airbnb, Inc.': 'ABNB', 'Airbus SE': 'air.pa_data', 'Akzo Nobel N.V.': 'AKZOY', 'Alexandria Real Estate Equities, Inc.': 'ARE', 'Alibaba Group Holding Limited': 'BABA', 'Alphabet Inc. (Google)': 'GOOGL', 'Amazon Inc.': 'amzn_data', 'Amazon.com, Inc.': 'AMZN', 'Ambev S.A.': 'ABEV', 'America Movil, S.A.B. de C.V.': 'AMX', 'American Electric Power Company, Inc.': 'AEP', 'American Tower Corporation': 'AMT', 'Amgen Inc.': 'AMGN', 'Anheuser-Busch InBev SA/NV': 'BUD', 'Apple Inc.': 'aapl_data', 'Applied Materials, Inc.': 'AMAT', 'ArcelorMittal': 'MT', 'AstraZeneca PLC': 'AZN', 'Autodesk, Inc.': 'ADSK', 'Automatic Data Processing, Inc.': 'ADP', 'BASF SE': 'BASFY', 'BBVA (Banco Bilbao Vizcaya Argentaria, S.A.)': 'BBVA', 'BHP Group': 'BHP', 'BMW AG': 'BMWYY', 'BNP Paribas S.A.': 'bnp.pa_data', 'Baidu, Inc.': 'BIDU', 'Baker Hughes Company': 'BKR', 'Banco Bradesco S.A.': 'BBD', 'Banco Santander, S.A.': 'SAN', 'Bank of America Corporation': 'BAC', 'Bank of China Limited': 'BACHY', 'Barclays Africa Group Limited': 'AGRPY', 'Bayer AG': 'BAYRY', 'Berkshire Hathaway Inc.': 'BRK.A', 'BioNTech SE': 'BNTX', 'Biogen Inc.': 'BIIB', 'Bitcoin': 'btc-usd_data', 'Booking Holdings Inc.': 'BKNG', 'Bristol Myers Squibb Company': 'BMY', 'CME Group Inc.': 'CME', 'Campbell Soup Company': 'CPB', 'Canon Inc.': 'CAJ', 'Capgemini SE': 'cap.pa_data', 'Celgene Corporation': 'CELG', 'Cemex, S.A.B. de C.V.': 'CX', 'Charles Schwab Corporation': 'SCHW', 'Chevron Corporation': 'CVX', 'China Construction Bank Corporation': 'CICHY', 'China Life Insurance Company Limited': 'LFC', 'China Mobile Limited': 'CHL', 'China Southern Airlines Company Limited': 'ZNH', 'Cisco Systems, Inc.': 'CSCO', 'Clorox Company': 'CLX', 'Coca-Cola Company': 'ko_data', 'Coinbase Global, Inc.': 'COIN', 'Colgate-Palmolive Company': 'CL', 'Columbia Sportswear Company': 'COLM', 'Comcast Corporation': 'CMCSA', 'Constellation Brands, Inc.': 'STZ', 'Crown Castle International Corp.': 'CCI', 'Deere & Company': 'DE', 'Diageo plc': 'DEO', 'Digital Realty Trust, Inc.': 'DLR', 'Dollar General Corporation': 'DG', 'Dominion Energy, Inc.': 'D', 'DoorDash, Inc.': 'DASH', 'Dow Inc.': 'DOW', 'DraftKings Inc.': 'DKNG', 'DuPont de Nemours, Inc.': 'DD', 'Duke Energy Corporation': 'DUK', 'Eaton Corporation plc': 'ETN', 'Ebay Inc.': 'EBAY', 'Ecolab Inc.': 'ECL', 'Eli Lilly and Company': 'LLY', 'Embraer S.A.': 'ERJ', 'Equinix, Inc.': 'EQIX', 'Estée Lauder Companies Inc.': 'EL', 'Etsy, Inc.': 'ETSY', 'Exelon Corporation': 'EXC', 'Expedia Group, Inc.': 'EXPE', 'Exxon Mobil Corporation': 'XOM', 'Facebook, Inc. (Meta Platforms, Inc.)': 'FB', 'FedEx Corporation': 'FDX', 'Ferrari N.V.': 'RACE', 'Fomento Económico Mexicano, S.A.B. de C.V. (FEMSA)': 'FMX', 'Garmin Ltd.': 'GRMN', 'General Dynamics Corporation': 'GD', 'General Mills, Inc.': 'GIS', 'Gilead Sciences, Inc.': 'GILD', 'GlaxoSmithKline plc': 'GSK', 'Glencore PLC': 'GLNCY', 'GoPro, Inc.': 'GPRO', 'Goldman Sachs Group, Inc.': 'GS', 'Google LLC': 'goog_data', 'Groupe AXA': 'cs.pa_data', 'Groupe Carrefour': 'ca.pa_data', 'Groupe Danone': 'bn.pa_data', 'Grupo Bimbo, S.A.B. de C.V.': 'GRBMF', 'Grupo Televisa, S.A.B.': 'TV', 'HSBC Holdings plc': 'HSBC', 'Halliburton Company': 'HAL', 'Henkel AG & Co. KGaA': 'HENKY', 'Hermès International': 'rms.pa_data', 'Honeywell International Inc.': 'HON', 'Huawei Technologies Co., Ltd.': 'Not Publicly Traded', 'ICBC (Industrial and Commercial Bank of China)': 'IDCBY', 'ING Groep N.V.': 'ING', 'Iberdrola, S.A.': 'IBDRY', 'Illumina, Inc.': 'ILMN', 'Inditex SA': 'IDEXY', 'Infosys Limited': 'INFY', 'Intel Corporation': 'INTC', 'International Business Machines Corporation': 'IBM', 'International Flavors & Fragrances Inc.': 'IFF', 'Intuit Inc.': 'INTU', 'Intuitive Surgical, Inc.': 'ISRG', 'Itaú Unibanco Holding S.A.': 'ITUB', 'JD.com, Inc.': 'JD', 'JPMorgan Chase & Co.': 'JPM', 'Johnson & Johnson': 'JNJ', 'KLA Corporation': 'KLAC', 'Kellogg Company': 'K', "L'Oréal S.A.": 'or.pa_data', "L'Oréal SA": 'LRLCY', 'LVMH': 'mc.pa_data', 'Lam Research Corporation': 'LRCX', 'Li Auto Inc.': 'LI', 'Linde plc': 'LIN', 'Lockheed Martin Corporation': 'LMT', 'Lucid Group, Inc.': 'LCID', 'Lululemon Athletica Inc.': 'LULU', 'Luxottica Group SpA': 'LUX', 'Lyft, Inc.': 'LYFT', 'LyondellBasell Industries NV': 'LYB', 'MTN Group Limited': 'MTNOY', 'Marathon Petroleum Corporation': 'MPC', 'Mastercard Incorporated': 'MA', "McDonald's Corporation": 'MCD', 'Meituan Dianping': 'MPNGY', 'Meta Platforms Inc.': 'meta_data', 'Michelin SCA': 'ml.pa_data', 'Micron Technology, Inc.': 'MU', 'Microsoft Corporation': 'msft_data', 'Moderna, Inc.': 'MRNA', 'Monster Beverage Corporation': 'MNST', 'Morgan Stanley': 'MS', 'NIO Inc.': 'NIO', 'NVIDIA Corporation': 'nvda_data', 'Naspers Limited': 'NPSNY', 'Nautilus, Inc.': 'NLS', 'Nestlé SA': 'NSRGY', 'Netflix Inc.': 'nflx_data', 'Netflix, Inc.': 'NFLX', 'NextEra Energy, Inc.': 'NEE', 'Nike, Inc.': 'NKE', 'Nikon Corporation': 'NINOY', 'Northrop Grumman Corporation': 'NOC', 'Novartis AG': 'NVS', 'Oracle Corporation': 'ORCL', 'Orange S.A.': 'ora.pa_data', 'PPG Industries, Inc.': 'PPG', 'Palantir Technologies Inc.': 'PLTR', 'Panasonic Corporation': 'PCRFY', 'Peloton Interactive, Inc.': 'PTON', 'PepsiCo, Inc.': 'PEP', 'PetroChina Company Limited': 'PTR', 'Petrobras (Petróleo Brasileiro S.A.)': 'PBR', 'Pfizer Inc.': 'PFE', 'Phillips 66': 'PSX', 'Ping An Insurance (Group) Company of China, Ltd.': 'PNGAY', 'Praxair, Inc.': 'PX', 'Procter & Gamble Company': 'PG', 'Prologis, Inc.': 'PLD', 'Public Storage': 'PSA', 'Puma SE': 'PUMSY', 'Qualcomm Incorporated': 'QCOM', 'Raytheon Technologies Corporation': 'RTX', 'Reckitt Benckiser Group plc': 'RBGLY', 'Regeneron Pharmaceuticals, Inc.': 'REGN', 'Reliance Industries Limited': 'RELIANCE.NS', 'Remgro Limited': 'RMGRY', 'Renault S.A.': 'rno.pa_data', 'Richemont (Compagnie Financière Richemont SA)': 'CFRUY', 'Rio Tinto Group': 'RIO', 'Rivian Automotive, Inc.': 'RIVN', 'Robinhood Markets, Inc.': 'HOOD', 'Roblox Corporation': 'RBLX', 'Roche Holding AG': 'RHHBY', 'Roku, Inc.': 'ROKU', 'Ross Stores, Inc.': 'ROST', 'S&P Global Inc.': 'SPGI', 'SAP SE': 'SAP', 'Salesforce.com, Inc.': 'CRM', 'Samsung Electronics Co., Ltd.': '005930.KS', 'Sanofi S.A.': 'san.pa_data', 'Sasol Limited': 'SSL', 'Schlumberger Limited': 'SLB', 'Seagate Technology PLC': 'STX', 'ServiceNow, Inc.': 'NOW', 'Sharp Corporation': 'SHCAY', 'Sherwin-Williams Company': 'SHW', 'Shopify Inc.': 'SHOP', 'Shoprite Holdings': 'SRHGY', 'Siemens AG': 'SIEGY', 'Siemens Healthineers AG': 'SEMHF', 'Simon Property Group, Inc.': 'SPG', 'Sinopec (China Petroleum & Chemical Corporation)': 'SNP', 'Snowflake Inc.': 'SNOW', 'Société Générale S.A.': 'gle.pa_data', 'SoftBank Group Corp.': 'SFTBY', 'Sony Corporation': 'SNE', 'Sony Group Corporation': 'SONY', 'Southern Company': 'SO', 'Square, Inc.': 'SQ', 'Steinhoff International Holdings': 'SNH', 'TJX Companies Inc.': 'TJX', 'Taiwan Semiconductor Manufacturing Company Limited': 'TSM', 'Tata Motors Limited': 'TTM', 'Telefónica S.A.': 'TEF', 'Tencent Holdings Ltd.': 'TCEHY', 'Tesla Inc.': 'tsla_data', 'Tesla, Inc.': 'TSLA', 'Texas Instruments Incorporated': 'TXN', 'Thales Group': 'ho.pa_data', 'The Boeing Company': 'BA', 'The Home Depot, Inc.': 'HD', 'The Kraft Heinz Company': 'KHC', 'The Walt Disney Company': 'dis_data', 'Toshiba Corporation': 'TOSYY', 'TotalEnergies SE': 'tte.pa_data', 'Toyota Motor Corporation': 'TM', 'Uber Technologies, Inc.': 'UBER', 'UiPath Inc.': 'PATH', 'Under Armour, Inc.': 'UAA', 'Unilever PLC': 'UL', 'United Parcel Service, Inc.': 'UPS', 'UnitedHealth Group Incorporated': 'UNH', 'Unity Software Inc.': 'U', 'VF Corporation': 'VFC', 'VINCI S.A.': 'dg.pa_data', 'VMware, Inc.': 'VMW', 'Vale S.A.': 'VALE', 'Valero Energy Corporation': 'VLO', 'Ventas, Inc.': 'VTR', 'Veolia Environnement SA': 'vie.pa_data', 'Verizon Communications Inc.': 'VZ', 'Vertex Pharmaceuticals Incorporated': 'VRTX', 'Visa Inc.': 'v_data', 'Volkswagen AG': 'VWAGY', 'Walgreens Boots Alliance, Inc.': 'WBA', 'Walmart Inc.': 'WMT', 'Welltower Inc.': 'WELL', 'Western Digital Corporation': 'WDC', 'Wipro Limited': 'WIT', 'XPeng Inc.': 'XPEV', 'Zoom Video Communications, Inc.': 'ZM'};
 
   Future<void> loadJsonData(String fileName) async {
     final String symbol = fileName.replaceAll('_data', '').toUpperCase();
@@ -205,7 +172,7 @@ class _bourse_dashboardState extends State<bourse_dashboard> {
               Spacer(),
               SizedBox(
                 child: Text(
-                    '${_bestPercentageChange.abs().toStringAsFixed(2)}%',
+                    '${_bestPercentageChange.abs().toStringAsFixed(2)}% ',
                     style: TextStyle(
                         fontSize: 25,
                         color: _bestPercentageChange >= 0 ? Colors.green : Colors.red
@@ -223,7 +190,7 @@ class _bourse_dashboardState extends State<bourse_dashboard> {
               Spacer(),
               SizedBox(
                 child: Text(
-                    '${_worstPercentageChange.abs().toStringAsFixed(2)}%',
+                    '${_worstPercentageChange.abs().toStringAsFixed(2)}% ',
                     style: TextStyle(
                         fontSize: 25,
                         color: _worstPercentageChange >= 0 ? Colors.green : Colors.red
